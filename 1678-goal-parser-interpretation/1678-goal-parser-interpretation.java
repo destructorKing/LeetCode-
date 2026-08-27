ced8bd02a37477ca517c6373347ea25f044524bb
destructorKing <87614985+destructorKing@users.noreply.github.com>
2026-08-27 18:11:25 +0530

    Time: 1 ms (74.99%), Space: 43.1 MB (33%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,31 @@
 class Solution {
     public String interpret(String command) 
     {
-        return command.replace("()","o").replace("(al)","al");    
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < command.length(); i++)
+        {
+            if(command.charAt(i) == 'G')
+                {
+                    result.append('G');
+                }
+            else if(command.charAt(i) == '(')
+            {
+                if(command.charAt(i+1) == ')')
+                {
+                    result.append('o');
+                    i++; 
+                }
+            
+            else 
+                {
+                    result.append('a');
+                    result.append('l');
+                    i += 3;
+                }
+            }
+        } 
+
+        return result.toString();  
     }
 }
