964ac9e184bb2386de919c7c4b0b53917e0461ca
destructorKing <87614985+destructorKing@users.noreply.github.com>
2026-09-19 10:48:01 +0530

    Time: 52 ms (7.88%), Space: 52.7 MB (86.85%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,8 +2,9 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if(!this.length) return -1
-    return this.at(-1)
+    if(this.length === 0)
+        return -1;
+    return this[this.length-1];
 };
 
 /**
